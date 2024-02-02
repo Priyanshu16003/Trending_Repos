938f54ef80033f4bd3f70b9452c7964f99d986ac
Priyanshu <priyanshutandel7@gmail.com>
2024-02-02 11:27:23 +0530

    added swipe refresh view, shimmer effect, created no internet screens

--- app/src/main/java/com/example/trendingrepos/MainActivity.kt
+++ app/src/main/java/com/example/trendingrepos/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.trendingrepos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import com.facebook.shimmer.ShimmerFrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val shimmer = findViewById<ShimmerFrameLayout>(R.id.shimmer_layout)
+        shimmer.startShimmer()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
