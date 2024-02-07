73e9c636368496f61268bafa7eab1f3392f5273c
Priyanshu <priyanshutandel7@gmail.com>
2024-02-07 13:51:39 +0530

    implemented heart onClick

--- app/src/main/java/com/example/trendingrepos/adapters/GitHubRepoCardRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/trendingrepos/adapters/GitHubRepoCardRecyclerViewAdapter.kt
@@ -3,6 +3,7 @@ package com.example.trendingrepos.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -30,7 +31,8 @@ class GitHubRepoCardRecyclerViewAdapter(private val response: Repos?) : Recycler
         val starCount : TextView
         val forkCount : TextView
         val color : CardView
-
+        val heart : ImageView
+        var isHeart : Boolean
         init {
             moreDetails = view.findViewById(R.id.more_details)
             login = view.findViewById(R.id.name)
@@ -41,6 +43,8 @@ class GitHubRepoCardRecyclerViewAdapter(private val response: Repos?) : Recycler
             starCount = view.findViewById(R.id.star_count)
             forkCount = view.findViewById(R.id.fork_count)
             color = view.findViewById(R.id.color)
+            heart = view.findViewById(R.id.heart_icon)
+            isHeart = false
         }
     }
 
@@ -48,6 +52,10 @@ class GitHubRepoCardRecyclerViewAdapter(private val response: Repos?) : Recycler
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.github_card_layout, viewGroup, false)
         return ViewHolder(view)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return RecyclerView.NO_ID
     }
 
     override fun getItemCount(): Int {
@@ -61,6 +69,17 @@ class GitHubRepoCardRecyclerViewAdapter(private val response: Repos?) : Recycler
         viewHolder.language.text = response.items[position].language
         viewHolder.starCount.text = response.items[position].stargazers_count.toString()
         viewHolder.forkCount.text = response.items[position].forks_count.toString()
+
+        viewHolder.heart.setOnClickListener{
+            if (!viewHolder.isHeart){
+                viewHolder.isHeart = true
+                viewHolder.heart.setImageResource(R.drawable.ic_heart_filled)
+            }
+            else{
+                viewHolder.isHeart = false
+                viewHolder.heart.setImageResource(R.drawable.ic_heart)
+            }
+        }
 
 
         response.items[position].owner.login.let { login ->
